e1b9435eaff0c0b907d4f6501010fb65e142b111
Ethan Jackson <ethantjackson44@gmail.com>
2024-07-21 22:47:59 -0700

    Resolve warnings

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
-import { Box, Button, Container, Typography } from '@mui/material';
 import IntroSplash from './IntroSplash';
-import Navbar from './Navbar';
 import IntroNav from './IntroNav';
 
 function App() {
   return (
     <div>
       <IntroNav />
-      {/* <Navbar /> */}
       <IntroSplash />
     </div>
   );
