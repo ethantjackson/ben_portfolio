93d17707c112fd11ca5c9ca76165e4b6423582e5
Ben Thomas <bcpeters100@gmail.com>
2025-08-14 21:33:20 -0700

    Added first two projects

--- src/BEN_UPDATE_INFO_HERE/ProjectsInfo.js
+++ src/BEN_UPDATE_INFO_HERE/ProjectsInfo.js
@@ -10,14 +10,15 @@ export const PROJECTS_INFO = [
     title: '2025 Reel',
     thumbnailTimeSeconds: 1,
     thumbnailURL:
-      'https://res.cloudinary.com/du0rugjrg/video/upload/v1755215547/2025Reel_ThumbnailVideo_vjlcum.mp4',
+      'https://res.cloudinary.com/du0rugjrg/video/upload/v1755226891/2025Reel2_ThumbnailVideo_ypde3b.mp4',
     description:
-      'Some highlights from my work over the past couple of years.',
+      'Some highlights from my work over the past couple of years, including 2D, 3D and hand-painted animation. For more specific information on roles, see below.',
     credits: {
 
     },
     detailsContent: [
       {
+        contentType: CONTENT_TYPE.VIDEO_EMBED,
         vimeoID: '1104531613',
         width: 12, // accepts values from 1 to 12
       },
@@ -25,54 +26,106 @@ export const PROJECTS_INFO = [
     ],
   },
 
-  // YERBA MATTE
+  // Kinetic Type
   {
-    title: 'YERBA MATTE',
-    thumbnailTimeSeconds: 1,
+    title: 'Madness/Genius',
+    thumbnailTimeSeconds: 3,
     thumbnailURL:
-      'https://res.cloudinary.com/workoutcloud/video/upload/v1722488383/YerbaMate_MainVideo_yaa37n.mp4',
+      'https://res.cloudinary.com/du0rugjrg/video/upload/v1754585965/RideTheLine_ThumbnailVideo_zarmdc.mp4',
     description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, illo perspiciatis repellat delectus tenetur eum in neque rem Lorem eligendi inventore ut earum?',
+      'As a part of Bubba\'s LA\'s rebrand, I designed and animated a kinetic type treatment of the studio\'s catchphrase "Riding the line between madness and genius."',
     credits: {
-      studio: 'Bing Chilling Labs',
-      director: 'Ben Peterson',
-      creativeDirection: 'Ethan Jackson',
+      studio: 'Bubbas LA',
+      creativeDirector: 'Ian Nelson',
+      DesignAndAnimation: 'Ben Peterson'
+      
     },
     detailsContent: [
       {
         contentType: CONTENT_TYPE.VIDEO_EMBED,
-        vimeoID: '1106766274',
-        width: 10, // accepts values from 1 to 12
+        vimeoID: '1106770278',
+        width: 12, // accepts values from 1 to 12
       },
       {
-        contentType: CONTENT_TYPE.STYLEFRAME,
-        url: 'https://res.cloudinary.com/workoutcloud/image/upload/v1726534625/instaStill1_wa0f4y.png',
-        width: 8, // accepts values from 1 to 12
-      },
-      {
-        contentType: CONTENT_TYPE.STYLEFRAME,
-        url: 'https://res.cloudinary.com/workoutcloud/image/upload/v1726534553/poster_z77luw.png',
-        width: 3,
-      },
-      {
-        contentType: CONTENT_TYPE.STYLEFRAME,
-        url: 'https://res.cloudinary.com/workoutcloud/image/upload/v1726534623/still2_ypknws.png',
-        width: 12,
+        contentType: CONTENT_TYPE.TEXT,
+        header: '',
+        value:
+          '',
       },
       {
         contentType: CONTENT_TYPE.TEXT,
         header: 'The Process',
         value:
-          'Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, illo perspiciatis repellat delectus tenetur eum in neque rem Lorem eligendi inventore ut earum?',
+          'Below are some exploratory styleframes I designed while concepting the project (including my mispelling of "genious" which made it further into the creative process than I\d like to admit).',
       },
+      {
+        contentType: CONTENT_TYPE.TEXT,
+        header: '',
+        value:
+          '',
+      },
+      {
+        contentType: CONTENT_TYPE.STYLEFRAME,
+        url: 'https://res.cloudinary.com/du0rugjrg/image/upload/v1754066448/KineticTypography_2.1_hklzs3.png',
+        width: 4, // accepts values from 1 to 12
+      },
+      {
+        contentType: CONTENT_TYPE.STYLEFRAME,
+        url: 'https://res.cloudinary.com/du0rugjrg/image/upload/v1754066444/KineticTypography_1.2_xv1kn5.png',
+        width: 4,
+      },
+      {
+        contentType: CONTENT_TYPE.STYLEFRAME,
+        url: 'https://res.cloudinary.com/du0rugjrg/image/upload/v1754066443/KineticTypography_1.1_vqrjyh.png',
+        width: 4,
+      },
+      {
+        contentType: CONTENT_TYPE.STYLEFRAME,
+        url: 'https://res.cloudinary.com/du0rugjrg/image/upload/v1754066441/KineticTypography_4_vubcbt.png',
+        width: 6,
+      },
+      {
+        contentType: CONTENT_TYPE.STYLEFRAME,
+        url: 'https://res.cloudinary.com/du0rugjrg/image/upload/v1754066434/KineticTypography_3.1_vxztpv.png',
+        width: 6,
+      },
+      {
+        contentType: CONTENT_TYPE.TEXT,
+        header: '',
+        value:
+          'More WIP styleframes and unused concepts that got left on the cutting room floor',
+      },
+      {
+        contentType: CONTENT_TYPE.TEXT,
+        header: '',
+        value:
+          '',
+      },
+      {
+        contentType: CONTENT_TYPE.STYLEFRAME,
+        url: 'https://res.cloudinary.com/du0rugjrg/image/upload/v1754066423/styleframe_Madness3_udlsvm.png',
+        width: 6,
+      },
+      {
+        contentType: CONTENT_TYPE.STYLEFRAME,
+        url: 'https://res.cloudinary.com/du0rugjrg/image/upload/v1754066421/styleframe_Madness2_esqpxf.png',
+        width: 6,
+      },
+      {
+        contentType: CONTENT_TYPE.STYLEFRAME,
+        url: 'https://res.cloudinary.com/du0rugjrg/image/upload/v1754066415/styleframe_Between2_eeafle.png',
+        width: 12,
+      },
+      
+
     ],
   },
-  // GUILT
+  // Pebbles
   {
-    title: 'GUILT',
-    thumbnailTimeSeconds: 6,
+    title: 'Pebbles',
+    thumbnailTimeSeconds: .5,
     thumbnailURL:
-      'https://res.cloudinary.com/workoutcloud/video/upload/v1722489624/Guilt_MT_WideFormatCC_v7lxev.mp4',
+      'https://res.cloudinary.com/du0rugjrg/video/upload/v1754585964/PebbleSim_ThumbnailVideo_a4bc1n.mp4',
     description:
       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, illo perspiciatis repellat delectus tenetur eum in neque rem Lorem eligendi inventore ut earum?',
     credits: {
