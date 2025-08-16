8f79413afb2ac9d1646f87dd0424b9e44c2fd7e0
Ben Thomas <bcpeters100@gmail.com>
2025-08-15 17:18:13 -0700

    Added experiments

--- src/BEN_UPDATE_INFO_HERE/ExperimentsInfo.js
+++ src/BEN_UPDATE_INFO_HERE/ExperimentsInfo.js
@@ -1,22 +1,53 @@
 export const EXPERIMENTS_INFO = [
   {
-    url: 'https://res.cloudinary.com/workoutcloud/image/upload/v1754290378/MonopolyGo_x6864o.png',
-    width: 7, // accepts values from 1 to 12
-    text: 'This was my favorite project ever. I experimented with new lighting techniques.',
-  },
-  {
-    url: 'https://res.cloudinary.com/workoutcloud/image/upload/v1754290476/CameraStill_rsjoem.png',
-    width: 3,
-    text: 'I made this rendering pipeline from scratch.',
-  },
-  {
-    url: 'https://res.cloudinary.com/workoutcloud/image/upload/v1754290790/PingPong_xku5te.png',
-    width: 3,
-    text: 'Getting the lighting right in this one was super hard...',
+    url: '',
+    width: 4,
+    text: '',
   },
   {
     url: 'https://res.cloudinary.com/workoutcloud/image/upload/v1754290759/02_HeadOnSmash_lwgamy.png',
-    width: 9,
-    text: "This project really sucked. I'm glad I finished it.",
+    width: 8,
+    text: 'This was an impact frame rendered with a toon shader I made in Cinema 4D. It was a lot of fun to really dig into pulling references from some of my favorite artists and animated projects in order to key in the look on this piece, and I hope to animate this project soon.',
   },
+  {
+    url: 'https://res.cloudinary.com/du0rugjrg/image/upload/v1754157371/03_ProfileSmash_xrfuoh.png',
+    width: 7,
+    text: "Both of these stills were also touched up in Photoshop, especially in designing the background text.",
+  },
+  {
+    url: '',
+    width: 4,
+    text: '',
+  },
+  {
+    url: 'https://res.cloudinary.com/du0rugjrg/image/upload/v1755301924/specRaspberry_ofe7sn.gif',
+    width: 5, // accepts values from 1 to 12
+    text: 'I made this when I was playing around with a condensation rig and I ended up learning a lot about food rendering and how important subsurface lighting is to making things look real/delicious.',
+  },
+  {
+    url: '',
+    width: 1,
+    text: '',
+  },
+  {
+    url: 'https://res.cloudinary.com/du0rugjrg/image/upload/v1755302237/RaspberryStill_p5zxzl.png',
+    width: 5,
+    text: 'No more lighting on this one >:( This was a happy accident when I got frustrated I couldn\'t get the shader right and ended up isolating the highlights, which I think turned out pretty cool!',
+  },
+  {
+    url: 'https://res.cloudinary.com/du0rugjrg/image/upload/v1755302531/PlanetProject_3_ncdmvg.gif',
+    width: 9,
+    text: 'Surprisingly this project was done entirely in After Effects with the Orb plugin. I really wanted to capture a sense of atmosphere and viscerally intense lighting with this one. If you want to see the full version with music check out my Vimeo!',
+  },
+  {
+    url: 'https://res.cloudinary.com/du0rugjrg/image/upload/v1754157524/Exploration_1_an8uh2.png',
+    width: 4,
+    text: 'Shout out Orb plugin',
+  },
+  {
+    url: 'https://res.cloudinary.com/du0rugjrg/image/upload/v1754157528/still7_gupbme.png',
+    width: 4,
+    text: '',
+  },
+  
 ];
